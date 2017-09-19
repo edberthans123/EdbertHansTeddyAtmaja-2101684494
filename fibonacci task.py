721b6a8858a55c50a044a43fd4e4aa8f7226926e
Edbert <edberthans@icloud.com>
2017-09-19 22:07:49 +0800

    task

---
+++ fibonacci task.py
@@ -0,0 +1,15 @@
+str ='mom'
+str = str.casefold()
+rev_str = reversed(str)
+if list(str) == list(rev_str):
+    print("it's a palindrome")
+else:
+    print("it's not a palindrome")
+
+str = 'popeye'
+str = str.casefold()
+rev_str = reversed(str)
+if list(str) == list(rev_str):
+    print("it's a palindrome")
+else:
+    print("it's not a palindrome")
